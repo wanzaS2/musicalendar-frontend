d39912ed38c15e6defffcc4bb2336576826b9050
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-04-02 00:19:36 +0900

    fullcalendar 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from "./components/Button";
-import {Calender} from "./components/Calender";
+import Calendar from "./components/Calendar";
 import {Main} from "./components/Main";
 import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
 
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
             <Routes>
                 <Route path={"/"} element={<Main/>}/>
-                <Route path={"/calender"} element={<Calender/>}/>
+                <Route path={"/calendar"} element={<Calendar/>}/>
             </Routes>
       </BrowserRouter>
   );
