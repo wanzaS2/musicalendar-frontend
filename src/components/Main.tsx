db42a8812ca2a01e5f5749578f3f5e44e37d3815
cielo0105 <cielo0105@naver.com>
2023-04-03 17:22:58 +0900

    메인화면

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,21 +1,36 @@
-import React from "react";
-import {useNavigate} from "react-router-dom";
+import React, {Component} from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "../logo.svg";
 import Button from "./Button";
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import ImagePicker from "./Slick";
+import Navbar from "../layout/Layout";
+import { NavBar } from "../layout/Navbar";
+
+
+
+
 export const Main = () => {
-    const navigate = useNavigate();
-    const goCalendar = () => {
-        navigate('/calendar');
-    }
-    return(
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-                Musicalendar Project Start!
-            </p>
-            <Button onClick={goCalendar}>Go!</Button>
-        </header>
-    )
+	const navigate = useNavigate();
+	const goCalendar = () => {
+		navigate('/calendar');
+	}
+	return (
+		<><Navbar />
+		<header>
+			<ImagePicker/>
+			{/* <img src={logo} className="App-logo" alt="logo" /> */}
+			<div className="btn">
+			<p>
+				Musicalendar Project Start!
+			</p>
+			
+			<Button onClick={goCalendar}>Go!</Button>
+			</div>
+		</header>
+		</>
+	)
 }
 
