a72d46f02b4da725396349aab73858b18ad787fe
yunDDi <67116186+yuna4490@users.noreply.github.com>
2023-05-02 17:52:30 +0900

    SiteSelectBox 추가

---
+++ src/components/SiteSelectBox.tsx
@@ -0,0 +1,19 @@
+function SiteSelectBox() {
+    return (
+        <select>
+            <option key="1" value="멜론티켓">
+                멜론티켓
+            </option>
+            <option key="2" value="예술의전당">예술의전당</option>
+            <option key="3" value="인터파크티켓토핑">인터파크티켓토핑</option>
+            <option key="4" value="인터파크티켓">인터파크티켓</option>
+            <option key="5" value="티켓링크">티켓링크</option>
+            <option key="6" value="예스24티켓">예스24티켓</option>
+            <option key="7" value="티몬">티몬</option>
+            <option key="8" value="위메프">위메프</option>
+            <option key="9" value="샤롯데씨어터">샤롯데씨어터</option>
+        </select>
+    );
+};
+
+export default SiteSelectBox;
